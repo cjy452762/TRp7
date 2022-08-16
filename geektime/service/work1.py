7473c7a211c32163ce1171e0bd3f5686520d9cad
cjy <shiyi1997415@163.com>
2022-08-16 22:26:44 +0800

    补充work1

--- geektime/service/work1.py
+++ geektime/service/work1.py
@@ -10,40 +10,42 @@ headers = {
 }
 
 
-# 使用变量储存用户信息
-userinfo = {}
-
-
 def test_work1():
     x = Work1()
-    x.dologin(userinfo)
+    userinfo = x.dologin()
     # print("登录用户=>", userinfo)
     # 首个商品
     goods = x.doList()
     # print("首个商品=>", goods)
     # 获取首个商品详情
-    productId = x.doDetail(goods)
-    print("首个商品详情=>", productId)
+    pro = x.doDetail(goods)
+    # print("首个商品详rodu情=>", pro)
+    # 获取购物车
+    beforeCarts = x.doCart()
+    # 添加购物车
+    x.doadd(pro)
+    # print("添加购物车成功")
+    afterCarts = x.doCart()
+    print('添加前商品数量：', beforeCarts['cartTotal']['goodsCount'])
+    print('添加后商品数量：', afterCarts['cartTotal']['goodsCount'])
+    assert afterCarts['cartTotal']['goodsCount'] > beforeCarts['cartTotal']['goodsCount']
+
 
 
 class Work1:
     # 登录接口
-    def dologin(self, userinfo):
+    def dologin(self):
         # 拼接登录url
         requestUrl = baseurl + '/wx/auth/login'
         body = json.dumps({"username": username,
                            "password": password
                            })
         response = requests.post(requestUrl, data=body, headers=headers)
-        # print(response.text)
-        # 登录成功
         assert response.status_code == 200
         res = json.loads(response.text)
-        # print(type(res))
-        # print( res['errmsg'])
         userinfo = res['data']
         headers['X-Litemall-Token'] = userinfo['token']
-        # print('headers=>', headers)
+        return userinfo
 
     # 商品搜索接口
     def doList(self):
@@ -59,13 +61,31 @@ class Work1:
         goods = res['data']['list'][0]
         return goods;
 
-    # 添加购物车接口
+    # 商品详情接口
     def doDetail(self, goodsInfo):
-        # print('goods detail=>', goodsInfo)
         requestUrl = "%s/wx/goods/detail?id=%d" % (baseurl, goodsInfo['id'])
         response = requests.get(requestUrl, data='', headers=headers)
         assert response.status_code == 200
         res = json.loads(response.text)
         productId = res['data']['specificationList'][0]['valueList'][0]
-        return productId
         print('productId=>', res['data']['specificationList'][0]['valueList'][0])
+        return productId
+
+    # 添加购物车接口
+    def doadd(self, pro):
+        requestUrl = baseurl + '/wx/cart/add'
+        body = json.dumps({
+            "goodsId": pro['goodsId'],
+            "number": 1,
+            "productId": pro['id']
+        })
+        response = requests.post(requestUrl, data=body, headers=headers)
+        res = json.loads(response.text)
+        assert response.status_code == 200
+        assert res['errno'] == 0
+
+    def doCart(self):
+        requesUrl = baseurl + '/wx/cart/index'
+        response = requests.get(requesUrl, headers=headers)
+        res = json.loads(response.text)
+        return res['data']
