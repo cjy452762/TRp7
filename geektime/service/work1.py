038724c3f956b19955a4e6e242823a9c915cecbe
cjy452762 <shiyi1997415@163.com>
2022-08-16 14:50:23 +0800

    新的项目

--- geektime/service/work1.py
+++ geektime/service/work1.py
@@ -8,25 +8,43 @@ password = "123456"
 headers = {
     "Content-Type": "application/json;charset=UTF-8"
 }
+# 使用变量储存用户信息
+userinfo = {}
 
-userinfo={}
 
 def test_work1init():
     x = Work1()
     x.dologin(userinfo)
+    x.test_doTest()
+
 
 class Work1:
-    def dologin(self,userinfo):
+    def dologin(self, userinfo):
+        # 拼接登录url
         requestUrl = baseurl + '/wx/auth/login'
-        print(requestUrl)
         body = json.dumps({"username": username,
                            "password": password
                            })
         response = requests.post(requestUrl,
                                  data=body, headers=headers)
-        print(response.text)
+        # print(response.text)
+        # 登录成功
         assert response.status_code == 200
         responseData = json.loads(response.text)
-        print(type(responseData))
+        # print(type(responseData))
         userinfo = responseData['data']
-        print('userinfo=>', userinfo)
+        headers['X-Litemall-Token'] = userinfo['token']
+        # print('headers=>', headers)
+
+    def test_doTest(self):
+        requestUrl = baseurl + '/wx/goods/list'
+        body = json.dumps({"keyword": "3D",
+                           "page": 1,
+                           "limit": 10
+                           })
+        response = requests.post(requestUrl,
+                                 data=body, headers=headers)
+
+        assert response.status_code == 200
+        responseData = json.loads(response.text)
+        print(responseData)
